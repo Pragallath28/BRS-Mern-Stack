5eee7995cf8caba1c21503dcd355aef381aa3029
Pragallath <53934292+Pragallath28@users.noreply.github.com>
2022-08-11 14:56:36 +0530

    Update on backend

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -2,7 +2,7 @@
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const User = require('../models/User')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
 
 
